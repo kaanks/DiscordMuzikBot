d02214a6856a162721c222b2229f59f2d2fce9b4
Kaan KÖSE <kaan1097@gmail.com>
2024-06-06 16:17:23 +0300

    Add EasyCommand

---
+++ src/main/java/tr/keanks/Main.java
@@ -0,0 +1,28 @@
+package tr.keanks;
+
+import ca.tristan.easycommands.EasyCommands;
+import ca.tristan.easycommands.commands.defaults.HelpCmd;
+import ca.tristan.testbot.ExampleCommand;
+import net.dv8tion.jda.api.JDA;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.EventListener;
+
+public class Main {
+    private static JDA jda;
+    public static void main(String[] args) throws IOException, URISyntaxException, InterruptedException {
+
+        EasyCommands easyCommands = new EasyCommands();
+
+        easyCommands.getConfig().getToken();
+
+        jda = easyCommands.registerListeners(
+
+        ).addExecutor(
+                new HelpCmd(easyCommands)
+
+        ).addEnabledCacheFlags().addGatewayIntents().buildJDA();
+
+    }
+}
